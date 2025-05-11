294e230e1f56a0c7d71c26a3ab6806b8919dc1b2
Md Sohanur Rahman Shawon <73399259+shawon00real@users.noreply.github.com>
2025-05-11 11:04:03 +0600

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	     $('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    autoplay:true,
+	    autoplayTimeout:3000,
+	    dots:false,
+	    nav:false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+
+});
